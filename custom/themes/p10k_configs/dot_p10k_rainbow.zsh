38fadfbc5cbb37956193305415004803f97f2697
Janine <janine.cheng@gmail.com>
2020-06-18 00:13:37 -0700

    Squashed commit from dev: powerline config updates

--- custom/themes/p10k_configs/dot_p10k_rainbow.zsh
+++ custom/themes/p10k_configs/dot_p10k_rainbow.zsh
@@ -44,6 +44,7 @@
       newline
       docker_machine
       kubecontext             # current kubernetes context (https://kubernetes.io/)
+      pyenv
       prompt_char             # prompt symbol
   )
 
@@ -57,9 +58,9 @@
       command_execution_time  # duration of the last command
       background_jobs         # presence of background jobs
       direnv                  # direnv status (https://direnv.net/)
-      virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-      anaconda                # conda environment (https://conda.io/)
-      pyenv                   # python environment (https://github.com/pyenv/pyenv)
+      # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+      # anaconda                # conda environment (https://conda.io/)
+      # pyenv                   # python environment (https://github.com/pyenv/pyenv)
       goenv                   # go environment (https://github.com/syndbg/goenv)
       nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
       nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
